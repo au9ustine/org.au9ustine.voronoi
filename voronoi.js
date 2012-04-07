a8c4e1d7a24b8829e0081433e65343dcf8ea4049
Tianchen Shao <duke.augustine@gmail.com>
2012-04-07 12:51:04 +0800

    add socket.io dep

---
+++ voronoi.js
@@ -0,0 +1,14 @@
+function Point(a,b){
+    this.x = (a)?a:0;
+    this.y = (b)?b:0;
+}
+
+var d = [], q = [];
+
+d.push(1,2,new Point(),new Point(1,2));
+q.push(2);
+
+
+console.log(d,d.length);
+console.log(q,q.length);
+
